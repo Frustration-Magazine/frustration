4b17f3d41880150b3127e25c4a4ff85042860382
Nicolas Alibert <122890983+nicolasalibert@users.noreply.github.com>
2025-06-05 17:40:07 +0200

    ✨ feat(web): add authors page

--- apps/web/src/components/H3.tsx
+++ apps/web/src/components/H3.tsx
@@ -10,6 +10,7 @@ function H3({ className, children }: Props) {
     <h3
       className={cn(
         "mb-2 font-bold text-balance",
+        "font-open-sans",
         "text-xl",
         "sm:text-2xl",
         "lg:text-3xl",
